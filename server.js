0365df51a6428c49479b4cfd4c7b6c53118a109d
Omolewa Stephen Ayobami <Omolewastephen@gmail.com>
2018-06-01 21:43:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-/*********************************************************************************
-* WEB322 – Assignment 02
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
-* of this assignment has been copied manually or electronically from any other source 
-* (including 3rd party web sites) or distributed to other students.
-* 
-* Name: ______________________ Student ID: ______________ Date: ________________
-*
-* Online (Heroku) Link: ________________________________________________________
-*
-********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express =   require('express');
 var app =       express();
